bfaec87b1091bb3ee4dbab4d144788877fccb9ce
jordansgrant <jordans.grant@gmail.com>
2017-08-27 22:48:40 -0600

    Added Two Opt functions to Tour.rb

--- classes/tour.rb
+++ classes/tour.rb
@@ -20,6 +20,11 @@ module TSP
       @distance
     end
 
+
+
+    #############################################
+    #          Genetic Functions
+    #############################################
     def mutate!(rate)
       @route.each_with_index do |location, index|
         if rand(0.0..1.0) < rate
@@ -32,6 +37,35 @@ module TSP
       @distance = nil
       @fitness = nil
     end
+    ######## End Genetic Functions ##############
+
+    #############################################
+    #         Two Opt Functions
+    #############################################
+    
+    # performs a transform operation for the Two Opt Algorithm
+    # copies region from 0 to point1-1 and point2+1 to the end in order
+    # copies region from point1 to point2 in reverse
+    def two_opt_transform(point1, point2)
+      new_tour = Tour.new(@route.length)
+      clone_region_forward(new_tour, 0, point1 - 1)
+      clone_region_reverse(new_tour, point1, point2)
+      clone_region_forward(new_tour, point2 + 1, new_tour.length - 1)
+      new_tour
+    end
+
+    # Clones the region between start_index and stop_index in order into
+    # new_tour
+    def clone_region_forward(new_tour, start_index, stop_index)
+      new_tour[(start_index..stop_index)] = @route[(start_index..stop_index)]
+    end
+
+    # clones the region between start_index and stop_index in reverse into
+    # new_tour
+    def clone_region_reverse(new_tour, start_index, stop_index)
+      new_tour[(start_index..stop_index)] = @route[(start_index..stop_index)].reverse
+    end
+    ######## End Two Opt Functions ##############
 
     def method_missing(method, *args)
       if (Array.instance_methods.include?(method) || Enumerable.instance_methods.include?(method))
